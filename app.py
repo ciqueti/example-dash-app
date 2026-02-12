42a0f88446256abe667442c5102b9a78df37e753
Leonardo Chiqueti <leonardo92006@gmail.com>
2026-02-12 13:02:06 -0300

    Controls and Callbacks

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import packages
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, callback, Output, Input
 import dash_ag_grid as dag
 import pandas as pd
 import plotly.express as px
@@ -12,14 +12,24 @@ app = Dash()
 
 # App Layout
 app.layout = [
-    html.Div(children="My First App with Data"),
+    html.Div(children="My First App with Data, Graph, and Controls"),
+    html.Hr(),
+    dcc.RadioItems(options=["pop", "lifeExp", "gdpPercap"], value="lifeExp", id="controls-and-radio-item"),
     dag.AgGrid(
         rowData=df.to_dict("records"),
         columnDefs=[{"field": i} for i in df.columns]
     ),
-    dcc.Graph(figure=px.histogram(df, x="continent", y="lifeExp", histfunc="avg"))
+    dcc.Graph(figure={}, id="controls-and-graph")
 ]
 
+# Add controls to build the interaction
+@callback(
+    Output(component_id="controls-and-graph", component_property="figure"),
+    Input(component_id="controls-and-radio-item", component_property="value")
+)
+def update_graph(col_chosen):
+    fig = px.histogram(df, x="continent", y=col_chosen, histfunc="avg")
+    return fig
 
 # Run the App
 if __name__ == "__main__":
