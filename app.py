5154fe05a379dc4c688253ca221c01e8ce1a2606
Leonardo Chiqueti <leonardo92006@gmail.com>
2026-02-12 12:11:10 -0300

    Visualizing Data

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # Import packages
-from dash import Dash, html
+from dash import Dash, html, dcc
 import dash_ag_grid as dag
 import pandas as pd
+import plotly.express as px
 
 # Incorporate data
 df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv")
@@ -15,7 +16,8 @@ app.layout = [
     dag.AgGrid(
         rowData=df.to_dict("records"),
         columnDefs=[{"field": i} for i in df.columns]
-    )
+    ),
+    dcc.Graph(figure=px.histogram(df, x="continent", y="lifeExp", histfunc="avg"))
 ]
 
 
