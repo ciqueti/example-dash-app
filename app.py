afd598495fb3110851ffa2791ff99388ee0a8881
Leonardo Chiqueti <leonardo92006@gmail.com>
2026-02-17 20:27:31 -0300

    Plotly Dash Course - Sessino 3 - Part 1

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-# Plotly Dash Course - Session 2
+# Plotly Dash Course - Session 3 - Part 1
 # Callbacks -> Multiple Outputs and Multiple Inputs
 
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 import dash_ag_grid as dag
-import json
 
 # Incorporate data
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
@@ -18,15 +17,13 @@ app = Dash(__name__)
 
 # App layout
 app.layout = html.Div([
-    html.Div(children='My First App with Data, Graph, and Controls', id="html-div"),
+    html.Div(children='My First App with Data, Graph, and Controls'),
     html.Hr(),
-    dcc.Dropdown(options=["NYC", "MTL", "SF"], value="NYC", id="demo-dropdown"),
     dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='column-options'),
     dag.AgGrid(
         id="grid",
         rowData=df.to_dict("records"),
         columnDefs=[{"field": i} for i in df.columns],
-        dashGridOptions={"rowSelection" : "single"}
     ),
     dcc.Graph(figure=fig, id='graph1')
 ])
@@ -34,23 +31,11 @@ app.layout = html.Div([
 # Add controls to build the interaction
 @callback(
     Output(component_id='graph1', component_property='figure'),
-    Output(component_id="html-div", component_property="children"),
-
-    Input(component_id='column-options', component_property='value'),
-    Input(component_id="demo-dropdown" ,component_property="value"),
-    Input(component_id="grid" , component_property="selectedRows")
+    Input(component_id='column-options', component_property='value')
 )
-
-def update_graph(radio_btn, dropdown_value, selected_row): # Each args function is gonna be from an Input callback, in the right order
-
-    # print(selected_row[0]["country"])
-    print(df[df["country"] == selected_row[0]["country"]])
-
-    html_div = f"Drowpdown selected {dropdown_value}"
-
-    fig = px.histogram(df, x='continent', y=radio_btn, histfunc='avg')
-
-    return fig, html_div  # The return object will be placed/assigned in the Output property, in the right order
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    return fig
 
 
 # Run the app
