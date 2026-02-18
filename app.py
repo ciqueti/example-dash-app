d3f8354a03222394c2db977c29db5523e1fb178b
Leonardo Chiqueti <leonardo92006@gmail.com>
2026-02-18 12:49:42 -0300

    Plotly Dash Course - Session 4 - Part 3 -> State

--- app.py
+++ app.py
@@ -1,40 +1,50 @@
 # Plotly Dash Course - Session 4 - Part 2 -> State
-# State Callbacks
+# State Callbacks -> Let's assume we want multiples components to be selected (dropdown, radio-button, date-picker) before updating the graph (activate the callback)
 
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
+from dash import Dash, html, dcc, callback, Input, Output, State, no_update
+import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
-import dash_ag_grid as dag
 
 # Incorporate data
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
-# Plotly graphs
-fig = px.histogram(df, x='continent', y='pop', histfunc='avg')
-
 # Initialize the app
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])
 
 # App layout
-app.layout = html.Div([
-    html.Div(children='My First App with Data, Graph, and Controls'),
+app.layout = dbc.Container([
+    html.H1(children='Country Analysis'),
     html.Hr(),
-    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='column-options'),
-    dag.AgGrid(
-        id="grid",
-        rowData=df.to_dict("records"),
-        columnDefs=[{"field": i} for i in df.columns],
-    ),
-    dcc.Graph(figure=fig, id='graph1')
+    
+    dbc.Row([
+        dbc.Col([
+            dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='yaxis-options'),   
+        ], width=6),
+        dbc.Col([
+            dcc.Dropdown(options=['country', 'continent'], value='continent', id='xaxis-options'),    
+        ], width=6)
+    ], className='mb-3'),
+
+    dbc.Button("Submit", id="my-button", n_clicks=0),
+
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(figure={}, id='graph1')
+        ], width=12)
+    ]),
 ])
 
 # Add controls to build the interaction
 @callback(
     Output(component_id='graph1', component_property='figure'),
-    Input(component_id='column-options', component_property='value')
+    Input(component_id="my-button", component_property="n_clicks"),
+    State(component_id='yaxis-options', component_property='value'),
+    State(component_id='xaxis-options', component_property='value'),
+    
 )
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+def update_graph(_, y_chosen, x_chosen):
+    fig = px.histogram(df, x=x_chosen, y=y_chosen, histfunc='avg')
     return fig
 
 
