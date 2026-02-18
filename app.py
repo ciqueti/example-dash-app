642c5674d4fcebb1a353470b082df7ed75116998
Leonardo Chiqueti <leonardo92006@gmail.com>
2026-02-18 12:30:54 -0300

    Plotly Dash Course - Session 4 - Part 1 -> Layout

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-# Plotly Dash Course - Session 4 - Setup
+# Plotly Dash Course - Session 4 - Part 1 - Layout
 # Dash Bootstrap Components
+
+# KeyWords SizeScreen Behavior -> xs, sm, md, lg, xl, xxl
 
 from dash import Dash, html
 import dash_bootstrap_components as dbc
@@ -9,9 +11,18 @@ app = Dash(
 )
 
 app.layout = dbc.Container([
-    html.H1("Dash Bootstrap Alerts"),
-    dbc.Alert("Hello, Bootstrap!", className="m-5", is_open=True, duration=4000)
+    dbc.Row([
+        dbc.Col(dbc.Alert("One of three columns"), width=4),
+        dbc.Col(dbc.Alert("2nd of three columns"), width=4),
+        dbc.Col(dbc.Alert("3rd of three columns"), width=4)
+    ]),
+    dbc.Row([
+        dbc.Col(dbc.Alert("One of four columns"), width=6, lg=3, md=2),
+        dbc.Col(dbc.Alert("2nd of four columns"), width=6, lg=3, md =2),
+        dbc.Col(dbc.Alert("3rd of four columns"), width=6, lg=3, md=2),
+        dbc.Col(dbc.Alert("4th of four columns"), width=6, lg=3, md=2)
+    ])
 ])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
